f89e37ecb0a830e7eda95a8e77be72bd897c0a5d
cmtrkr83 <forumcemturker@gmail.com>
2026-03-02 12:50:38 +0300

    77

--- server.js
+++ server.js
@@ -336,10 +336,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use((req, res, next) => {
-    res.setHeader('ngrok-skip-browser-warning', 'true');
-    next();
-});
+
 
 // Uploads klasörünü oluştur
 const uploadsDir = path.join(__dirname, 'uploads');
